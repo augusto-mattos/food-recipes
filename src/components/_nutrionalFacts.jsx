ba2977a4d93e03e84ac939fb9fbdc3b7ffb6e8ca
augusto-mattos <guto.s.mattos@gmail.com>
2024-04-08 15:18:48 +0200

    add : nutritional facts

---
+++ src/components/_nutrionalFacts.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+function NutritionalFacts() {
+  const storedRecipe = JSON.parse(sessionStorage.getItem("recipeData"))?.recipe || {};
+  const { totalNutrients } = storedRecipe;
+  const [showAll, setShowAll] = useState(false);
+
+  const limitedNutrients = Object.keys(totalNutrients).slice(0, 18);
+
+  return (
+    <>
+      <div className="nutritional-facts">
+        <h4>Nutritional Facts</h4>
+        <div className="nutritional-items">
+        {limitedNutrients.map((nutrientLabel, index) => (
+            <div key={index}>
+              <p>
+                <strong>{totalNutrients[nutrientLabel].label}: </strong>
+              </p>
+              <p>
+                {parseFloat(totalNutrients[nutrientLabel].quantity).toFixed(2)}{" "}
+                {totalNutrients[nutrientLabel].unit}
+              </p>
+            </div>
+          ))}
+          {!showAll && Object.keys(totalNutrients).length > 18 && (
+            <button onClick={() => setShowAll(true)}>Ver mais</button>
+          )}
+          {showAll && Object.keys(totalNutrients).slice(18).map((nutrientLabel, index) => (
+            <div key={index}>
+              <p>
+                <strong>{totalNutrients[nutrientLabel].label}: </strong>
+              </p>
+              <p>
+                {parseFloat(totalNutrients[nutrientLabel].quantity).toFixed(2)}{" "}
+                {totalNutrients[nutrientLabel].unit}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NutritionalFacts;
