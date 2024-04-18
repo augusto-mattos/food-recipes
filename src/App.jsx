e16abf6ba58350e65faf265a9f05f0f2c8abc2ca
augusto-mattos <guto.s.mattos@gmail.com>
2024-04-18 11:08:38 +0200

    fix : nome do usuario ao logar com email e senha

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/_layout';
-import Home from './pages/Home';
-import Recipe from './pages/Recipe';
-import Login from './pages/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/_layout";
+import Home from "./pages/Home";
+import Recipe from "./pages/Recipe";
+import Login from "./pages/Login";
 import { AuthGoogleProvider } from "../src/contexts/authGoogle";
 
-import './assets/styles.css';
+import "./assets/styles.css";
 
 function App() {
   return (
     <AuthGoogleProvider>
-    <Router>
-      <Layout> 
+      <Router>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path='/recipe/:id' element={<Recipe />} />
-          <Route path='/login' element={<Login />} />
+          <Route
+            index
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path="/recipe/:id"
+            element={
+              <Layout>
+                <Recipe />
+              </Layout>
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
         </Routes>
-      </Layout> 
-    </Router>
+      </Router>
     </AuthGoogleProvider>
   );
 }
