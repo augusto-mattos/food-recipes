6a321cb873313cd8ee59e792eef64f8a3359291f
augusto-mattos <guto.s.mattos@gmail.com>
2024-05-03 16:16:53 +0200

    condicao para exibir receitas favoritas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,20 @@
+import { useContext } from "react";
+import { AuthGoogleContext } from "../../contexts/authGoogle";
+
 import Hero from "../../components/_hero";
 import FeaturedRecipes from "../../components/_featuredRecipes";
 import AdvancedSearch from "../../components/_advancedSearch";
 import FavoriteRecipes from "../../components/_favoriteRecipes";
 
 function Home() {
+  const { signed } = useContext(AuthGoogleContext);
+
   return (
     <>
       <Hero />
       <FeaturedRecipes />
       <AdvancedSearch />
-      <FavoriteRecipes />
+      {signed && <FavoriteRecipes />}
     </>
   );
 }
