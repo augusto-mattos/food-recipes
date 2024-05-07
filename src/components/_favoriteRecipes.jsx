910f6e6192f6e66da6a167334272b8cc059b3b44
augusto-mattos <guto.s.mattos@gmail.com>
2024-05-07 10:02:35 +0200

    secao com lista de favoritos

--- src/components/_favoriteRecipes.jsx
+++ src/components/_favoriteRecipes.jsx
@@ -1,11 +1,56 @@
+import { useState, useEffect } from "react";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+
 function FavoriteRecipes() {
-    return (
-      <>
-      <h4>
-          Map com os cards de receitas que foram marcadas como favoritas. A secao so aparece para usuario que ja esteja logado
-      </h4>
-      </>
-    ) 
-  }
-  
-  export default FavoriteRecipes;
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [currentUser, setCurrentUser] = useState(null);
+  const db = getFirestore();
+
+  useEffect(() => {
+    const sessionUser = sessionStorage.getItem("@AuthFirebase:user");
+    if (sessionUser) {
+      const user = JSON.parse(sessionUser);
+      setCurrentUser(user);
+      fetchFavoriteRecipes(user.uid);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const fetchFavoriteRecipes = async (userId) => {
+    const docRef = doc(db, "favorites", userId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const favorites = docSnap.data();
+      if (favorites.recipes) {
+        setFavoriteRecipes(favorites.recipes);
+      } else {
+        setFavoriteRecipes([]);
+      }
+    } else {
+      setFavoriteRecipes([]);
+    }
+  };
+
+  return (
+    <section id="favoriteRecipes">
+      <div>
+        <h3>Suas receitas favoritas</h3>
+        <p>Consulte suas receitas favoritas</p>
+      </div>
+      <div className="favorites-recipes-list">
+        {favoriteRecipes.map((recipe) => (
+          <div key={recipe.id} className="favorite-recipe">
+            <img
+              src={recipe.image}
+              alt={recipe.label}
+              />
+              <h5>{recipe.label}</h5>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default FavoriteRecipes;
